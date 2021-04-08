cc708cc48192d552b35c52822c856cabbaa7d299
Cooper Young <cooperyoung@frontier.com>
2021-04-08 14:09:40 -0400

    added files

---
+++ functions.py
@@ -0,0 +1,28 @@
+from sklearn import datasets
+from joblib import load
+import numpy as np
+import json
+
+#load the model
+
+my_model = load('log_model.pkl')
+
+
+
+
+def my_prediction(data):
+
+  dummy = np.array(data)
+  dummyT = dummy.reshape(-1,1)
+  r = dummy.shape
+  t = dummyT.shape
+  r_str = json.dumps(r)
+  t_str = json.dumps(t)
+  predictions = my_model.predict(dummyT) 
+ #score = logisticRegr.score(x_test, y_test) #Computes final "accuracy score"
+#print(score)
+  name = death[predictions]
+  name = name.tolist()
+  name_str = json.dumps(name)
+  string = [t_str, r_str, name_str]
+  return string
