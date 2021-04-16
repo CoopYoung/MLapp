2b41755640f6ba42db467a1aaf88d7087d969986
Cooper Young <cooyoung@iu.edu>
2021-04-16 13:45:43 -0400

    more changes

--- functions.py
+++ functions.py
@@ -13,7 +13,7 @@ my_model = load('log_model.pkl')
 def my_prediction(data):
 
   dummy = np.array(data)
-  dummyT = dummy.reshape(-1,1)
+  dummyT = dummy.reshape(1,-1)
   r = dummy.shape
   t = dummyT.shape
   r_str = json.dumps(r)
@@ -21,8 +21,8 @@ def my_prediction(data):
   predictions = my_model.predict(dummyT) 
  #score = logisticRegr.score(x_test, y_test) #Computes final "accuracy score"
 #print(score)
-  name = death[predictions]
-  name = name.tolist()
-  name_str = json.dumps(name)
-  string = [t_str, r_str, name_str]
-  return string
+  #name = death[predictions]
+  #name = name.tolist()
+  #name_str = json.dumps(str(predictions[0]))
+  #string = [t_str, r_str, name_str]
+  return str(predictions[0])
